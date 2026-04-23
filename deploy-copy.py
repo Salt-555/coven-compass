092de282929e17ebf6a08362f6b4987d2ea8006f
salt-555 <seanalt555@gmail.com>
2026-04-23 15:27:42 -0600

    Add Meta Pixel PageView to landing + app pages; migrate to purchase-optimized campaign

--- deploy-copy.py
+++ deploy-copy.py
@@ -2,7 +2,7 @@
 """Upload the edited Coven Compass worker to Cloudflare."""
 import os, json, urllib.request
 
-CF_TOKEN = os.environ["CLOUDFLARE_TOKEN"]
+CF_TOKEN = os.getenv("CLOUDFLARE_TOKEN")
 ACCOUNT_ID = "52bd643c8e19504e500b44992b96e4a4"
 
 with open(os.path.join(os.path.dirname(__file__), "src", "worker.js")) as f:
@@ -12,6 +12,8 @@ metadata = {
     "main_module": "worker.js",
     "bindings": [
         {"type": "kv_namespace", "name": "SIGNUPS", "namespace_id": "719553a81859450596759c37f5727a7c"},
+        {"type": "kv_namespace", "name": "CUSTOMERS", "namespace_id": "8635044516fa443c9dd502e56d7d9a22"},
+        {"type": "kv_namespace", "name": "SALES", "namespace_id": "3756a16f13a54999a2d5d7e55981b6af"},
         {"type": "plain_text", "name": "BASE_URL", "text": "https://coven-compass.allmind.biz"},
     ]
 }
